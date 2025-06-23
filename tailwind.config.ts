23545391062dad3a5b09573eae9b24668b687ad5
Sathish <sathishkumar21102004@gmail.com>
2025-06-23 10:23:33 +0000

    curvy blob shapes, dont use sharp edges

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,6 +76,17 @@ export default {
         sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
+        'blob': {
+          '0%': {
+            'border-radius': '60% 40% 30% 70% / 60% 30% 70% 40%',
+          },
+          '50%': {
+            'border-radius': '30% 60% 70% 40% / 50% 60% 30% 60%',
+          },
+          '100%': {
+            'border-radius': '60% 40% 30% 70% / 60% 30% 70% 40%',
+          },
+        },
         'accordion-down': {
           from: {
             height: '0',
@@ -94,6 +105,7 @@ export default {
         },
       },
       animation: {
+        'blob': 'blob 8s ease-in-out infinite',
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
